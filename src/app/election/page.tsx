d258afe1a585ef5f9f27206232e9edb0b6dcbf44
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-22 20:40:32 +0100

    add electionPage

--- src/app/election/page.tsx
+++ src/app/election/page.tsx
@@ -1,11 +1,24 @@
+import { postElectionAction } from "@/features/election/actions";
+import { electionService } from "@/features/election/instance";
+
 export default async function Page() {
     // getAll elections and map all elections. if done -> show results, otherwise be able to close the election!
     // addNew election
     // patch/ set election as done!
+    const elections = await electionService.getAll()
 
     return (
       <>
       This is the election page!!
+      {elections.map((election) => {
+        return <p key={election.id}> {election.type}</p>
+      })}
+
+        <form action={postElectionAction}>
+        <input type="text" name="name" placeholder="election name here" className="input input-bordered w-full max-w-xs" />
+        <input type="text" name="type" placeholder="Type of election" className="input input-bordered w-full max-w-xs"/>
+        <button type="submit" className="btn btn-ghost">Send</button>
+        </form>
       </>
     );
   }
---
+++ src/features/election/actions.ts
@@ -0,0 +1,22 @@
+"use server";
+import { v4 as uuidv4 } from 'uuid';
+
+import { revalidatePath } from "next/cache";
+import { electionService } from "./instance";
+
+export const postElectionAction = async (formData: FormData) => {
+    const name = formData.get("name") as string;
+    const type = formData.get("type") as string;
+
+    const rawData = {
+        name,
+        type,
+        id: uuidv4(), 
+    }
+
+        await electionService.add(rawData);
+
+    revalidatePath("/representative");
+
+
+}
