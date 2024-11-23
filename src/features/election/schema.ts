02073b842aaa29fc7a406efaf991fb2a686188a0
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-23 14:04:06 +0100

    add votes table

--- src/features/election/schema.ts
+++ src/features/election/schema.ts
@@ -1,4 +1,5 @@
 import { boolean, integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { publicVotersTable } from "../representative/schema";
 
 export const electionTable = pgTable("election", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -7,9 +8,16 @@ export const electionTable = pgTable("election", {
 });
 
 export const alternativesTable = pgTable("alternatives", {
-  alternative_id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  alternative_id: integer().primaryKey().generatedAlwaysAsIdentity(), // ÄNDRA TILL VANLIGT ID SEN
   name: varchar().notNull(),
   election_id: integer()
     .notNull()
     .references(() => electionTable.id),
 });
+
+export const votesTable = pgTable("votes", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    voter_id: integer().notNull().references(() => publicVotersTable.id),
+    election_id: integer().notNull().references(() => electionTable.id),
+    alternative_id: integer().notNull().references(() => alternativesTable.alternative_id)  });
+
