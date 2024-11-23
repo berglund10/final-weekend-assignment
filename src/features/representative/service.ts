e393f5e4dbe01a636be2eb00781d1cb126a27232
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-23 10:15:29 +0100

    add validation for representative

--- src/features/representative/service.ts
+++ src/features/representative/service.ts
@@ -1,15 +1,18 @@
 import { Db } from "@/db/instance";
 import { representativeTable } from "./schema";
+import { Representative, representativeSchema } from "./validation";
 
 export const createService = (db: Db) => {
   return {
     getAll: async () => {
       return await db.select().from(representativeTable);
     },
-    add: async (data: any) => {
-      console.log(data);
-      return await db.push(data);
-    },
+    add: async (rawData: Representative) => {
+
+      const representative = representativeSchema.parse(rawData);
+
+      await db.insert(representativeTable).values(representative);
+},
     vote: async (id: string) => {
       console.log("VOTED FOR :" + id);
       //where id is === id. vote
---
+++ src/features/representative/validation.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const representativeSchema = z.object({
+    name: z.string(),
+    email: z.string()
+  });
+
+  export type Representative = z.infer<typeof representativeSchema>;
