9f86c9c19bd6dd2786eb8c50ab485d5771e15f93
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-24 12:44:42 +0100

    add calculateAgreementRate

---
+++ src/features/election/logic.ts
@@ -0,0 +1,20 @@
+export const calculateAgreementRate = (
+    representativeVotes: { voter_id: number | null; alternative_id: number }[],
+    publicPreferences: { voter_id: number | null; alternative_id: number }[]
+  ) => {
+
+    const validRepresentativeVotes = representativeVotes.filter(vote => vote.voter_id !== null);
+  
+    const matchingVotes = publicPreferences.filter((pref) => {
+      const representativeVote = validRepresentativeVotes.find(vote => vote.voter_id === pref.voter_id);
+      return representativeVote && representativeVote.alternative_id === pref.alternative_id;
+    });
+  
+    const matchingCount = matchingVotes.length;
+    
+    const totalVotes = validRepresentativeVotes.length;
+
+    const agreementRate = (matchingCount / totalVotes) * 100;
+  
+    return agreementRate;
+  };
