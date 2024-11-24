aeb55e12d89387b7f7d7b43e549681db207decd2
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-24 21:20:58 +0100

    more tests

--- src/features/election/logic.test.ts
+++ src/features/election/logic.test.ts
@@ -20,7 +20,7 @@ test("It should return 0 when no votes match", () => {
         { voter_id: 5, alternative_id: 2 },
       ];
   const result = calculateAgreementRate(representativeVotes, publicPreferences);
-  console.log(result)
+
   deepEqual(result, 0);
 });
 
@@ -44,6 +44,54 @@ test("Should filter and remove null, and give rate of 80 %", () => {
         { voter_id: null, alternative_id: 3 },
       ];
   const result = calculateAgreementRate(representativeVotes, publicPreferences);
-  console.log(result)
+
   deepEqual(result, 80);
+});
+
+test("It should return 100 when all votes match", () => {
+
+    const representativeVotes = [
+        { voter_id: 1, alternative_id: 1 },
+        { voter_id: 2, alternative_id: 1 },
+        { voter_id: 3, alternative_id: 1 },
+        { voter_id: 4, alternative_id: 1 },
+        { voter_id: 5, alternative_id: 1 },
+      ];
+      
+      const publicPreferences = [
+        { voter_id: 1, alternative_id: 1 },
+        { voter_id: 2, alternative_id: 1 },
+        { voter_id: 3, alternative_id: 1 },
+        { voter_id: 4, alternative_id: 1 },
+        { voter_id: 5, alternative_id: 1 },
+      ];
+  const result = calculateAgreementRate(representativeVotes, publicPreferences);
+
+  deepEqual(result, 100);
+});
+
+test("It should return 50 when half votes match", () => {
+
+    const representativeVotes = [
+        { voter_id: 1, alternative_id: 1 },
+        { voter_id: 2, alternative_id: 1 },
+        { voter_id: 3, alternative_id: 1 },
+        { voter_id: 4, alternative_id: 2 },
+        { voter_id: 5, alternative_id: 2 },
+        { voter_id: 5, alternative_id: 2 },
+
+      ];
+      
+      const publicPreferences = [
+        { voter_id: 1, alternative_id: 1 },
+        { voter_id: 2, alternative_id: 1 },
+        { voter_id: 3, alternative_id: 1 },
+        { voter_id: 4, alternative_id: 1 },
+        { voter_id: 5, alternative_id: 1 },
+        { voter_id: 5, alternative_id: 1 },
+
+      ];
+  const result = calculateAgreementRate(representativeVotes, publicPreferences);
+  
+  deepEqual(result, 50);
 });
