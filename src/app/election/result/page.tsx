e42d7654f387649d4bb966dab6135c148ac1b953
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-24 13:31:10 +0100

    add method to get electionName

--- src/app/election/result/page.tsx
+++ src/app/election/result/page.tsx
@@ -12,6 +12,7 @@ export default async function Page({
 
   const representatives = await representativeService.getAll();
   const voteCount = await electionService.getVoteCount(Number(id));
+  const electionName = await electionService.getElectionNameById(Number(id));
 
   const winner = voteCount.reduce((max, current) =>
     current.vote_count > max.vote_count ? current : max
@@ -20,13 +21,13 @@ export default async function Page({
   return (
     <div className="flex flex-col items-center justify-center p-8 space-y-8">
       <h1 className="text-4xl font-bold text-center text-gray-800">
-        Election Stats for Election ID: {id}
+        {electionName}
       </h1>
 
       <p className="text-xl text-center text-gray-600">
         Winner: <strong>{winner.name}</strong> with <strong>{winner.vote_count}</strong> votes!
       </p>
-      
+
       <div className="space-y-4">
         {representatives.map((rep) => {
           return (
