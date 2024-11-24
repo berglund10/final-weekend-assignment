48308750c35ab5a19d2f4b2842552efa8ba46621
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-24 15:17:36 +0100

    clean up representative service

--- src/features/representative/service.ts
+++ src/features/representative/service.ts
@@ -1,7 +1,6 @@
 import { Db } from "@/db/instance";
 import { publicVotersTable, representativeTable } from "./schema";
 import { Representative, representativeSchema } from "./validation";
-import { eq } from "drizzle-orm";
 
 export const createService = (db: Db) => {
   return {
@@ -17,34 +16,6 @@ export const createService = (db: Db) => {
       await db.insert(publicVotersTable).values({
         representative_id: id,
       });
-    },
-    getAllPublicVotersByRepresentative: async (id: number) => {
-      const publicVoters = await db
-        .select()
-        .from(publicVotersTable)
-        .where(eq(publicVotersTable.representative_id, id));
-
-      return publicVoters;
-    },
-
-    getPublicVotersForRepresentative: async (representative_id: number) => {
-      const voters = await db
-        .select()
-        .from(publicVotersTable)
-        .where(eq(publicVotersTable.representative_id, representative_id));
-
-      const representative = await db
-        .select()
-        .from(representativeTable)
-        .where(eq(representativeTable.id, representative_id))
-        .limit(1);
-
-      const result = voters.map((voter) => ({
-        ...voter,
-        representative: representative[0],
-      }));
-
-      return result;
     },
   };
 };
