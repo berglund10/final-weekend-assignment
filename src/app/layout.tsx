d40d2b8069ec97822a41a5bd70845837929c9b3a
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-24 13:10:10 +0100

    add link to representative and elections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html data-theme="corporate" lang="en">
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import Link from 'next/link';
+
 export default function Home() {
-  return <>This is the home page</>;
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
+      <h1 className="text-6xl font-bold mb-4">Welcome!</h1>
+      <p className="text-2xl mb-8">View representatives and ongoing elections – choose and vote!</p>
+      
+      <div className="flex space-x-4">
+      <Link href="/representative">
+          <button className="bg-blue-500 text-white py-2 px-6 rounded hover:bg-blue-600">
+            See Representatives
+          </button>
+        </Link>
+        <Link href="/election">
+          <button className="bg-green-500 text-white py-2 px-6 rounded hover:bg-green-600">
+            See Elections
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
