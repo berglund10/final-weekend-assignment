cf591ae504e08928133c856990e07e20aaa6427e
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-09 20:21:27 +0100

    add choicePage

--- src/app/public-preferences/choice/page.tsx
+++ src/app/public-preferences/choice/page.tsx
@@ -1,29 +1,9 @@
-import { electionService } from "@/features/election/instance";
-import { OptionDropdown } from "@/features/public-preferences/ui/option-dropdown";
-import { OptionElectionBoard } from "@/features/public-preferences/ui/option-election-board";
-import { representativeService } from "@/features/representative/instance";
+import ChoicePage from "@/features/public-preferences/ui/choice-page";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
-  const id = (await searchParams).id;
+type Props = {
+  searchParams: Promise<{ id?: string }>;
+};
 
-  const representative = await representativeService.getById(Number(id));
-  const elections = await electionService.getAllElections();
-
-  const publicVoters = await representativeService.getAllPublicVotersById(
-    Number(id),
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center p-8 space-y-8">
-      <h1 className="text-4xl font-bold text-center text-gray-800">
-        {representative.name}s voters
-      </h1>
-      <OptionDropdown publicVoters={publicVoters} />
-      <OptionElectionBoard elections={elections} />
-    </div>
-  );
+export default async function Page({ searchParams }: Props) {
+  return <ChoicePage searchParams={searchParams} />;
 }
---
+++ src/features/public-preferences/ui/choice-page.tsx
@@ -0,0 +1,29 @@
+import { electionService } from "@/features";
+import { representativeService } from "@/features";
+import { OptionDropdown } from "./option-dropdown";
+import { OptionElectionBoard } from "./option-election-board";
+
+type Props = {
+  searchParams: Promise<{ id?: string }>;
+};
+
+export default async function ChoicePage({ searchParams} : Props ) {
+  const id = (await searchParams).id;
+
+  const representative = await representativeService.getById(Number(id));
+  const elections = await electionService.getAllElections();
+
+  const publicVoters = await representativeService.getAllPublicVotersById(
+    Number(id),
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center p-8 space-y-8">
+      <h1 className="text-4xl font-bold text-center text-gray-800">
+        {representative.name}s voters
+      </h1>
+      <OptionDropdown publicVoters={publicVoters} />
+      <OptionElectionBoard elections={elections} />
+    </div>
+  );
+}
